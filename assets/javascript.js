31692b52f4dfcd28af19ad0f1e93c82d58f45136
S-Voss <stephen.voss24@gmail.com>
2017-07-15 16:32:46 -0400

    Added the moment.js code and pushed it into Firebase. Need to change formatting.

--- assets/javascript.js
+++ assets/javascript.js
@@ -11,29 +11,43 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $(document).ready(function(){
-  var nextArrival = "";
-  var minutesAway = "";
 
 	$("#add-train-btn").on("click", function(){
     event.preventDefault();
 
 		var trainName = $("#train-name-input").val().trim();
 		var destination = $("#destination-input").val().trim();
-    var frequency = moment($("#frequency-input").val().trim(), "mm").format("mm");
-		var firstTrain = moment($("#first-train-input").val().trim(), "hh:mm:ss").format("hh:mm:ss");
+    var frequency = $("#frequency-input").val().trim();
+		var firstTrain = moment($("#first-train-input").val().trim(), 'hh:mm');
 
-console.log(frequency);
-console.log(firstTrain);
+    console.log('frequency', frequency);
+    console.log('firstTrain', firstTrain);
+    var timeDiff = moment().diff(moment(firstTrain), "minutes");
+    console.log("DIFFERENCE IN TIME: " + timeDiff);
+    // Time apart (remainder)
+    var tRemainder = timeDiff % frequency;
+    console.log(tRemainder);
+    // Minute Until Train
+    var minutesAway = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minutesAway);
+    // Next Train
+    var nextArrival = moment().add(minutesAway, "minutes");
+    console.log(nextArrival);
+    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
 
-    database.ref().push({
+    var data = {
       trainName: trainName,
       destination: destination,
       frequency: frequency,
-      firstTrain: firstTrain,
-      nextArrival: nextArrival,
+      firstTrain: firstTrain.toISOString(),
+      nextArrival: nextArrival.toISOString(),
       minutesAway: minutesAway,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+    };
+
+    console.log(data);
+
+    database.ref().push(data);
 	});
 
 	database.ref().on("child_added", function(childSnapShot) {
@@ -47,47 +61,3 @@ console.log(firstTrain);
 	});
 
 });
-
-    // Solved Mathematically
-    // Test case 1:
-    // 16 - 00 = 16
-    // 16 % 3 = 1 (Modulus is the remainder)
-    // 3 - 1 = 2 minutes away
-    // 2 + 3:16 = 3:18
-
-    // Solved Mathematically
-    // Test case 2:
-    // 16 - 00 = 16
-    // 16 % 7 = 2 (Modulus is the remainder)
-    // 7 - 2 = 5 minutes away
-    // 5 + 3:16 = 3:21
-
-    // // Assumptions
-    // var tFrequency = 3;
-    //
-    // // Time is 3:30 AM
-    // var firstTime = "03:30";
-    //
-    // // First Time (pushed back 1 year to make sure it comes before current time)
-    // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
-    //
-    // // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    //
-    // // Difference between the times
-    // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-    //
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
-    //
-    // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-    //
-    // // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
